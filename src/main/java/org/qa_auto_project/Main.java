d18ece84330e6483b2ccdc4297bc6ca17bc8551d
Asset Ongarbayev <aset-ong@yandex.ru>
2023-03-15 16:59:09 +0600

    Initial commit

---
+++ src/main/java/org/qa_auto_project/Main.java
@@ -0,0 +1,15 @@
+package org.qa_auto_project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/asset/Documents/Automation QA/chromedriver/chromedriver");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.close();
+        
+    }
+}
