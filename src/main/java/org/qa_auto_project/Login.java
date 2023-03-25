6b4935cc710ac5def0469905d4037eb96eac4b19
Asset Ongarbayev <aset-ong@yandex.ru>
2023-03-25 12:16:26 +0600

    Adding test cases

--- src/main/java/org/qa_auto_project/Login.java
+++ src/main/java/org/qa_auto_project/Login.java
@@ -1,6 +1,5 @@
 package org.qa_auto_project;
 
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -62,8 +61,6 @@ public class Login {
 
         // Test Case 3: Empty username and password
         driver.get("https://www.saucedemo.com/");
-        WebElement usernameInput2 = driver.findElement(By.id("user-name"));
-        WebElement passwordInput2 = driver.findElement(By.id("password"));
         WebElement loginButton2 = driver.findElement(By.id("login-button"));
 
         loginButton2.click();
@@ -78,24 +75,46 @@ public class Login {
         }
         Thread.sleep(2000);
 
+        // Test Case 4: Locked-out user
+        driver.get("https://www.saucedemo.com/");
+        WebElement usernameInput3 = driver.findElement(By.id("user-name"));
+        WebElement passwordInput3 = driver.findElement(By.id("password"));
+        WebElement loginButton3 = driver.findElement(By.id("login-button"));
 
-        // Test Case 1: Valid login credentials
+        usernameInput3.sendKeys("locked_out_user");
+        passwordInput3.sendKeys("secret_sauce");
+        loginButton3.click();
+
+        errorMessage = driver.findElement(By.tagName("h3"));
+        expectedErrorMessage = "Epic sadface: Sorry, this user has been locked out.";
+        actualErrorMessage = errorMessage.getText();
+        if (actualErrorMessage.equals(expectedErrorMessage)) {
+            System.out.println("Test Case 4 Passed: Error message is displayed for locked-out user.");
+        } else {
+            System.out.println("Test Case 4 Failed: Error message is not displayed for locked-out user.");
+        }
+        Thread.sleep(2000);
+        // Test Case 5: Valid login credentials
         // enter valid username and password
-        usernameInput2.clear();
-        passwordInput2.clear();
-        usernameInput2.sendKeys("standard_user");
-        passwordInput2.sendKeys("secret_sauce");
-        loginButton2.click();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        WebElement usernameInput4 = driver.findElement(By.id("user-name"));
+        WebElement passwordInput4 = driver.findElement(By.id("password"));
+        WebElement loginButton4 = driver.findElement(By.id("login-button"));
+
+        usernameInput4.sendKeys("standard_user");
+        passwordInput4.sendKeys("secret_sauce");
+        loginButton4.click();
         Thread.sleep(2000);
         // verify that user is logged in and redirected to inventory page
         String expectedUrl = "https://www.saucedemo.com/inventory.html";
         String actualUrl = driver.getCurrentUrl();
-        driver.manage().window().maximize();
+
 
         if (actualUrl.equals(expectedUrl)) {
-            System.out.println("Test Case 4 Passed: User is logged in and redirected to the inventory page.");
+            System.out.println("Test Case 5 Passed: User is logged in and redirected to the inventory page.");
         } else {
-            System.out.println("Test Case 4 Failed: User was not redirected to the inventory page.");
+            System.out.println("Test Case 5 Failed: User was not redirected to the inventory page.");
         }
         // close the browser
         driver.quit();
