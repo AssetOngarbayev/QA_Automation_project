cc9c0e8ddb338d9e4b9c9f5e5213a557c1f1c717
Asset Ongarbayev <aset-ong@yandex.ru>
2023-04-13 15:37:58 +0600

    Adding test cases.

--- src/main/java/org/qa_auto_project/Cart.java
+++ src/main/java/org/qa_auto_project/Cart.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -27,8 +28,8 @@ public class Cart {
         loginButton4.click();
     }
 
-    @Test(priority = 0)
-    public void addToCart() throws InterruptedException {
+    @Test(priority = 1)
+    public void addToCartAndRemove() throws InterruptedException {
         WebElement product1 = driver.findElement(By.className("inventory_item_name"));
         String productName1 = product1.getText();
         WebElement addToCartButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
@@ -38,7 +39,54 @@ public class Cart {
         WebElement productInCart = driver.findElement(By.className("inventory_item_name"));
         String productName2 = productInCart.getText();
         Assert.assertEquals(productName2, productName1);
+        WebElement removeButton = driver.findElement(By.id("remove-sauce-labs-backpack"));
+        removeButton.click();
+        int productInCart2 = driver.findElements(By.className("inventory_item_name")).size();
+        Assert.assertTrue(productInCart2==0);
+    }
+    @Test(priority = 2)
+    public void continueShopping() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/inventory.html");
+        WebElement cartItemsCount = driver.findElement(By.className("shopping_cart_link"));
+        cartItemsCount.click();
+        WebElement continueShopping = driver.findElement(By.id("continue-shopping"));
+        continueShopping.click();
+        String inventoryPage = driver.getCurrentUrl();
+        Assert.assertEquals(inventoryPage, "https://www.saucedemo.com/inventory.html");
+    }
 
+    @Test(priority = 3)
+    public void checkoutPage() throws InterruptedException {
+        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        addToCartButton.click();
+        WebElement cartItemsCount = driver.findElement(By.className("shopping_cart_badge"));
+        cartItemsCount.click();
+        WebElement checkoutButton = driver.findElement(By.id("checkout"));
+        checkoutButton.click();
+        String checkoutPage = driver.getCurrentUrl();
+        Assert.assertEquals(checkoutPage, "https://www.saucedemo.com/checkout-step-one.html");
+        WebElement firstNameField = driver.findElement(By.id("first-name"));
+        WebElement lastNameField = driver.findElement(By.id("last-name"));
+        WebElement zipCodeField = driver.findElement(By.id("postal-code"));
+        Assert.assertTrue (firstNameField.isDisplayed() && lastNameField.isDisplayed() && zipCodeField.isDisplayed());
+        WebElement continueButton = driver.findElement(By.xpath("//input[@type='submit']"));
+        Assert.assertTrue(continueButton.isDisplayed());
+        WebElement cancelButton = driver.findElement(By.id("cancel"));
+        Assert.assertTrue(cancelButton.isDisplayed());
+
+    }
+
+    @Test(priority = 4)
+    public void cancelCheckout() throws InterruptedException {
+        WebElement cancelButton = driver.findElement(By.id("cancel"));
+        cancelButton.click();
+        String cartPage = driver.getCurrentUrl();
+        Assert.assertEquals(cartPage,"https://www.saucedemo.com/cart.html");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 
 }
